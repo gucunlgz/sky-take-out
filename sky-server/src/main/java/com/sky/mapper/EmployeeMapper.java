d36669f60f49c4d55f78c9248b833a39539edebd
lgz <3287363216@qq.com>
2024-11-27 20:59:53 +0800

    初始代码

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,15 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
+import com.sky.service.EmployeeService;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface EmployeeMapper {
@@ -12,7 +19,15 @@ public interface EmployeeMapper {
      * @param username
      * @return
      */
-    @Select("select * from employee where username = #{username}")
+    @Select("select * from sky_take_out.employee where username = #{username}")
     Employee getByUsername(String username);
 
+    @Insert("INSERT INTO sky_take_out.employee" +
+            "(id, name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user)" +
+            "VALUES (#{id},#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void addEmployee(Employee employee);
+
+    Page<Employee> pageSearch(EmployeePageQueryDTO employeePageQueryDTO);
+
+    void update(Employee employee);
 }
